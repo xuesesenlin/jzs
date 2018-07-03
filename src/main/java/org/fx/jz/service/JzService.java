ea72ed1bb8bf2ee7f0890501bab4ee930d1ce521
xuesemofa@126.com <xuesemofa@126.com>
2018-07-03 11:30:48 +0800

    添加打印服务

--- src/main/java/org/fx/jz/service/JzService.java
+++ src/main/java/org/fx/jz/service/JzService.java
@@ -4,6 +4,11 @@ import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.usermodel.Range;
 import org.fx.urils.GetUuid;
 
+import javax.print.*;
+import javax.print.attribute.DocAttributeSet;
+import javax.print.attribute.HashDocAttributeSet;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
 import java.io.*;
 import java.util.Map;
 
@@ -70,6 +75,34 @@ public class JzService {
                 e.printStackTrace();
             }
         }
+    }
+
+    /**
+     * 打印
+     */
+    public void print() {
+        File file = new File("D:\\xuesesenlin\\demo.doc");
+        PrintRequestAttributeSet pras = new HashPrintRequestAttributeSet();
+//设置打印格式，因为未确定文件类型，这里选择AUTOSENSE
+        DocFlavor flavor = DocFlavor.INPUT_STREAM.AUTOSENSE;
+//查找所有的可用打印服务
+        PrintService printService[] = PrintServiceLookup.lookupPrintServices(flavor, pras);
+//定位默认的打印服务
+        PrintService defaultService = PrintServiceLookup.lookupDefaultPrintService();
+//显示打印对话框
+        PrintService service = ServiceUI.printDialog(null, 200, 200, printService, defaultService, flavor, pras);
+
+//        if (service != null) {
+//            try {
+//                DocPrintJob job = service.createPrintJob(); //创建打印作业
+//                FileInputStream fis = new FileInputStream(file); //构造待打印的文件流
+//                DocAttributeSet das = new HashDocAttributeSet();
+//                Doc doc = new SimpleDoc(fis, flavor, das); //建立打印文件格式
+//                job.print(doc, pras); //进行文件的打印
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
 }
